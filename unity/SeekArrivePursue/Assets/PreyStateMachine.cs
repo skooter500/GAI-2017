a3150ca8f77e71437bbec421eefaeaf642881d45
Bryan Duggan <skooter500@gmail.com>
2017-03-29 10:44:44 +0100

    bla

---
+++ unity/SeekArrivePursue/Assets/PreyStateMachine.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolState:State
+{
+    public override void Enter()
+    {
+        fsm.GetComponent<FollowPath>().enabled = true;
+    }
+
+    public override void Exit()
+    {
+        fsm.GetComponent<FollowPath>().enabled = false;
+    }
+}
+
+public class AttackState : State
+{
+    public override void Enter()
+    {
+        fsm.GetComponent<OffsetPursue>().enabled = true;
+        fsm.GetComponent<OffsetPursue>().leader = 
+            GameObject.FindGameObjectWithTag("predator").GetComponent<Boid>();
+    }
+
+    public override void Exit()
+    {
+        fsm.GetComponent<OffsetPursue>().enabled = false;
+    }
+}
+
+public class PreyStateMachine : StateMachine {
+
+	// Use this for initialization
+	void Start () {
+        ChangeState(new PatrolState());
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+	}
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "predator")
+        {
+            ChangeState(new AttackState());
+        }
+    }
+
+}
---
+++ unity/SeekArrivePursue/Assets/StateMachine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State
+{
+    public StateMachine fsm;
+
+    public virtual void Enter() { }
+    public virtual void Exit() { }
+    public virtual void Think() { }
+
+}
+
+public class StateMachine : MonoBehaviour
+{
+
+    public State state;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Think());
+    }
+
+    System.Collections.IEnumerator Think()
+    {
+        while (true)
+        {
+            state.Think();
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    public void ChangeState(State newState)
+    {
+        if (state != null)
+        {
+            state.Exit();
+        }
+        newState = state;
+        newState.fsm = this;
+        newState.Enter();
+    }
+}
+
